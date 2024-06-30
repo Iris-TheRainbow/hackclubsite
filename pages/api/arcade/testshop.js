11bec47ca18aeb02c3265f358d6007e92d4a205a
iris <snazzsinclair@gmail.com>
2024-06-30 12:30:46 -0500

    Revert "what am i doinggg"
    This reverts commit dd0a9f96b327d5f0aac63952da47c5a15bc1f846.

---
+++ pages/api/arcade/testshop.js
@@ -0,0 +1,16 @@
+
+export default async function handler(req, res) {
+  const data = await shopParts()
+
+  const filteredData = data.filter(record => record["Enabled"]).map(record => {
+    return {
+      name: record['Name'],
+      smallName: record['Small Name'],
+      description: record['Description'],
+      hours: record['Cost Hours'],
+      imageURL: record['Image URL'],
+    }
+  })
+
+  return res.json(filteredData)
+}
---
+++ pages/arcade/testshop.js
@@ -0,0 +1,89 @@
+import ShopComponent from "../../components/arcade/shop-component"
+import { getArcadeUser } from "../api/arcade/[userAirtableID]"
+import { shopParts } from "../api/arcade/testshop"
+import { Image, Link, Text } from 'theme-ui'
+import { Balancer } from "react-wrap-balancer"
+import Meta from '@hackclub/meta'
+import Head from 'next/head'
+
+/** @jsxImportSource theme-ui */
+
+const styled = `
+@import url('https://fonts.googleapis.com/css2?family=Slackey&family=Emblema+One&family=Gaegu&display=swap');
+
+.slackey {
+  font-family: "Slackey", sans-serif;
+ }
+
+ .gaegu {
+  font-family: "Gaegu", sans-serif;
+}
+
+body {
+  background-color: #FAEFD6;
+}
+
+`
+
+export default function Shop({ availableItems, userAirtableID = null, hoursBalance = 0 }) {
+  
+  return (
+    <>
+      <Meta
+        as={Head}
+        title="Arcade Shop"
+        description="Redeem prizes at your own Arcade Shop."
+        image="https://cloud-luaw423i2-hack-club-bot.vercel.app/0frame_33__1_.png"
+      />
+      <style>
+        {`
+        ._title-container {
+          width: 100%;
+        }
+        `}
+      </style>
+      <Balancer className="_title-container">
+        <h1
+          sx={{
+            textAlign: 'center',
+            fontSize: 5,
+            color: '#FF8C37',
+            my: 0,
+            pt: 5,
+            display: 'block',
+            width: '100%'
+          }}
+          className="slackey"
+        >
+          Welcome to the shop
+        </h1>
+      </Balancer>
+      <Text sx={{ display: 'block', textAlign: 'center', color: '#35290F' }} className='gaegu' variant='subtitle' >Your current balance is {Math.floor(hoursBalance)} 🎟️</Text>
+      <ShopComponent availableItems={availableItems} userAirtableID={userAirtableID} hoursBalance={hoursBalance} />
+    </>
+  )
+}
+
+export async function getStaticProps( ) {
+  const props = ''
+  await Promise.all([
+    shopParts().then(items => {
+      const availableItems = items.filter(item => item['Enabled']).map(item => ({
+        'Name': 'testitem' || null,
+        'Small Name': 'test' || null,
+        'Full Name': 'testingitem' || null,
+        'Description': 'this item is a test' || null,
+        'Fulfillment Description': 'this itme is not fufilled' || null,
+        'Cost Hours': '9999' || 0,
+        id: 1,
+        'Image URL': 'https://1.gravatar.com/avatar/eff4705adec973404213b5e110132b4930a26144294b68a83fd9caf8c8ccdc72?size=256' || null,
+        'Max Order Quantity': '-1' || 1,
+        Stock: '-999' || null
+      }))
+      props.availableItems = item
+    }),
+    props.hoursBalance = 9999
+  ])
+  
+  return { props }
+}
